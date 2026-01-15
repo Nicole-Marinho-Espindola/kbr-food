78ba2cc00e166c44a20b6cfecd8cfdd4a09bb0db
Nicole-Marinho-Espindola <nicole.m.espindola@gmail.com>
2026-01-15 13:12:14 -0300

    fixing build

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,6 +3,11 @@ import axios from "axios";
 
 export const api = axios.create({
     baseURL: 'http://ec2-18-219-222-98.us-east-2.compute.amazonaws.com',
+    timeout: 30000, 
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+    },
 });
 
 api.interceptors.request.use(async (config) => {
@@ -11,4 +16,23 @@ api.interceptors.request.use(async (config) => {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
+}, (error) => {
+  console.error('Erro na requisição:', error);
+  return Promise.reject(error);
 });
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      console.error('Erro na resposta da API:', error.response.status, error.response.data);
+    } else if (error.request) {
+      console.error('Erro de conexão - sem resposta do servidor:', error.message);
+    } else {
+      console.error('Erro ao configurar requisição:', error.message);
+    }
+    return Promise.reject(error);
+  }
+);
